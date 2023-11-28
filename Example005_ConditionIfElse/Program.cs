d17e0455641b85375e4169d4eccbe465befd93c1
Konstantin <kostya13111997@rambler.ru>
2023-11-28 20:54:45 +0300

    IfElse

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("введите имя пользователя ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "Masha")
+{
+    Console.WriteLine("Ура, это же МАША");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+
+}
